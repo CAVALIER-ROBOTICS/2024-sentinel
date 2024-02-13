818320d06b87ab4f41c69be918785154cbd14000
noahjedi10 <noahjedi10robot@hotmail.com>
2024-02-13 09:25:29 -0600

    finished climb ready for testing

--- src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -11,37 +11,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 public class ClimbSubsystem extends SubsystemBase {
+  
   CANSparkMax leftClimb = new CANSparkMax(Constants.LEFT_CLIMB, MotorType.kBrushless);
   CANSparkMax rightClimb = new CANSparkMax(Constants.RIGHT_CLIMB, MotorType.kBrushless);
 
   /** Creates a new ClimbSubsystem. */
   public ClimbSubsystem() {
-    leftClimb.setInverted(Constants.LEFT_INTAKE_DIRECTION);
-    rightClimb.setInverted(Constants.RIGHT_INTAKE_DIRECTION);
+
+    leftClimb.setInverted(false);
+    rightClimb.setInverted(true);
   }
 
-  public void setInverted()
-  {
-     leftClimb.setInverted(!leftClimb.getInverted());
-     rightClimb.setInverted(!rightClimb.getInverted());
-  }
-  public void setNormal()
-  {
-    leftClimb.setInverted(Constants.LEFT_INTAKE_DIRECTION);
-    rightClimb.setInverted(Constants.RIGHT_INTAKE_DIRECTION);
-  }
+  public void setLeft(double percent) {
 
-
-  
-  public void setLeftPercentOutput(double percent) {
     leftClimb.set(percent);
   } 
 
-  public void setRightPercentOutput(double percent)
-  {
-    rightClimb.set(percent);
-  }
+  public void setRight(double percent) {
 
+     rightClimb.set(percent);
+ }
 
   @Override
   public void periodic() {}
