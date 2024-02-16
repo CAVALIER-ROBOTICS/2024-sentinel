119c1f4f219b354b790b15317ba6128f56d2672d
3x71us1v3 <you@example.com>
2024-02-16 09:48:09 -0800

    I hate rev2mdistancesensor!!!!!!!!

--- src/main/java/frc/robot/subsystems/IntakeSubsystem.java
+++ src/main/java/frc/robot/subsystems/IntakeSubsystem.java
@@ -2,6 +2,8 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
+//If you have issues with the rev2mdistancesensor install, please reinstall it locally
+// https://github.com/REVrobotics/2m-Distance-Sensor 
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
