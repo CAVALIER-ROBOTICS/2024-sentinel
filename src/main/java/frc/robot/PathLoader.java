f8026d2269a4056a7c854f9d808339e096cdae56
3x71us1v3 <you@example.com>
2024-02-17 15:49:31 -0800

    Added pathfind to position method

--- src/main/java/frc/robot/PathLoader.java
+++ src/main/java/frc/robot/PathLoader.java
@@ -8,6 +8,7 @@ import java.util.function.Consumer;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.commands.PathPlannerAuto;
+import com.pathplanner.lib.path.PathConstraints;
 import com.pathplanner.lib.path.PathPlannerPath;
 import com.pathplanner.lib.util.HolonomicPathFollowerConfig;
 import com.pathplanner.lib.util.PIDConstants;
@@ -15,6 +16,8 @@ import com.pathplanner.lib.util.ReplanningConfig;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.DriveSubsystem;
@@ -55,6 +58,16 @@ public class PathLoader {
                 hpfc,
                 () -> getShouldFlipPath(),
                 driveSub);
+    }
+
+    public static Command pathfindToPosition(Pose2d position) { //Make sure navgrid.json is in /deploy/pathplanner
+        PathConstraints constraints = new PathConstraints(1.5, 3, Math.toRadians(540), Math.toRadians(720));
+        Command path = AutoBuilder.pathfindToPose(position, constraints, 0, 0);
+        return path;
+    }
+
+    public static void configureDynamicObstacles(Pose2d[] botObstacleCenters, Translation2d currentBotPos) {
+        
     }
 
     public static Command loadPath(String name) {
